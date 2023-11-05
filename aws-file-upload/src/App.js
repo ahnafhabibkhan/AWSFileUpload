dc9828e2c23909367b69b8d2ddb887f10727b699
ahnafhabibkhan <ahnafkhan70@gmail.com>
2023-11-05 15:12:51 -0500

    Changes in App.js

--- aws-file-upload/src/App.js
+++ aws-file-upload/src/App.js
@@ -16,27 +16,44 @@ class App extends Component {
   onFileUpload = () => {
     this.setState({ errorMessage: '' }); // Clear any previous error message
 
-    const formData = new FormData();
-    formData.append('file', this.state.selectedFile, this.state.selectedFile.name);
+    if (this.state.selectedFile) {
+      const formData = new FormData();
+      formData.append('file', this.state.selectedFile, this.state.selectedFile.name);
 
-    // Make the API request using axios
-    axios
-      .post("https://sk48x8co83.execute-api.us-east-1.amazonaws.com/prod/file-upload", formData)
-      .then(() => {
-        this.setState({ selectedFile: null });
-        this.setState({ fileUploadedSuccessfully: true });
-      });
+      // Set the custom Content-Type header
+      const config = {
+        headers: {
+          'Content-Type': 'application/pdf',
+        },
+      };
+
+      // Make the POST request using axios with the specified endpoint and content type
+      axios
+        .post("https://bsi6jz1ywb.execute-api.us-east-1.amazonaws.com/v1/upload", formData, config)
+        .then(() => {
+          this.setState({ selectedFile: null });
+          this.setState({ fileUploadedSuccessfully: true });
+        })
+        .catch((error) => {
+          this.setState({ errorMessage: error.message }); // Handle any errors
+        });
+    } else {
+      this.setState({ errorMessage: 'Please choose a file before uploading.' });
+    }
   };
 
   fileData = () => {
     if (this.state.selectedFile) {
+      // Create a Date object from lastModified property
+      const lastModified = new Date(this.state.selectedFile.lastModified);
+      
       return (
         <div>
           <h2>File Details:</h2>
           <p>File Name: {this.state.selectedFile.name}</p>
           <p>File Type: {this.state.selectedFile.type}</p>
           <p>
-            Last Modified: {this.state.selectedFile.lastModifiedDate.toDateString()}
+            Last Modified: {lastModified.toDateString()}
           </p>
         </div>
       );
@@ -57,13 +74,9 @@ class App extends Component {
           )}
         </div>
       );
-    }
+    };
   };
-
-  componentDidMount() {
-    // Use componentDidMount for setting up event listeners
-    document.getElementById('upload-button').addEventListener('click', this.onFileUpload);
-  }
+  
 
   render() {
     return (
@@ -72,7 +85,7 @@ class App extends Component {
         <h3>Upload your File with React and Serverless API on the go!</h3>
         <div>
           <input type="file" onChange={this.onFileChange} />
-          <button id="upload-button">Upload</button>
+          <button id="upload-button" onClick={this.onFileUpload}>Upload</button>
         </div>
         {this.fileData()}
       </div>
